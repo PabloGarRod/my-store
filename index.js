3d1ce5a3008df069369db9d9dcc183d65007a471
Pablo <pablogarcirodriguez@gmail.com>
2023-08-22 17:46:44 +0200

    Clase 4: Routing con Express.js

--- index.js
+++ index.js
@@ -8,6 +8,37 @@ app.get('/', (req, res) => {
   res.send('Hola mi server express');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola soy nueva ruta');
+});
+
+app.get('/productos', (req, res) => {
+  res.json({
+    name: 'producto 1',
+    precio: 1000,
+  });
+});
+
+app.get('/home', (req, res) => {
+  res.send('BIENVENIDO A LA PÁGINA DE INICIO');
+});
+
+app.get('/categorias', (req, res) => {
+  res.json({
+    muebles: {
+      cocina: 100,
+      baño: 200,
+      sala: 400,
+    },
+    electrodomesticos: {
+      television: 50,
+      lavadora: 60,
+      microondas: 70,
+      nevera: 150,
+    },
+  });
+});
+
 app.listen(port, () => {
   console.log('Mi port:', port);
 });
