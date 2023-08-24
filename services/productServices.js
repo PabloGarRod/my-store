5734d7630f04d337131db9d44fe163273f6cf30c
Pablo <pablogarcirodriguez@gmail.com>
2023-08-24 18:21:11 +0200

    Clase 15: Create, update and delete

--- services/productServices.js
+++ services/productServices.js
@@ -18,7 +18,15 @@ class ProductsServices {
     }
   }
 
-  create() {}
+  create(data) {
+    const newProduct = {
+      id: faker.string.uuid(),
+      ...data,
+    };
+
+    this.products.push(newProduct);
+    return newProduct;
+  }
 
   find() {
     return this.products;
@@ -28,7 +36,24 @@ class ProductsServices {
     return this.products.find((item) => item.id === id);
   }
 
-  delete() {}
+  update(id, changes) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error('product not found');
+    }
+    const product = this.products[index];
+    this.products[index] = { ...product, ...changes };
+    return this.products[index];
+  }
+
+  delete(id) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error('product not found');
+    }
+    this.products.splice(index, 1);
+    return { id };
+  }
 }
 
 module.exports = ProductsServices;
