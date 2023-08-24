ead824a3ec7a044fffa0b399110f79c5b5b97afa
Pablo <pablogarcirodriguez@gmail.com>
2023-08-24 19:46:08 +0200

    Clase 18: Middlewar para HttpErrors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routerApi = require('./routes');
 
+const { logErros, errorHandler } = require('./middlewares/errorHandler');
+
 const app = express();
 
 const port = 3050;
@@ -24,3 +26,6 @@ app.listen(port, () => {
 });
 
 routerApi(app);
+
+app.use(logErros);
+app.use(errorHandler);
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,15 @@
+function logErros(err, req, res, next) {
+  console.log('logErrors');
+  console.log(err);
+  next(err);
+}
+
+function errorHandler(err, req, res, next) {
+  console.log('errorHandler');
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+}
+
+module.exports = { logErros, errorHandler };
